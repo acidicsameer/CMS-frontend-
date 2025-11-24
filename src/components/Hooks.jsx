af53885dd5d9afa726323b7ead07e92abc0c4727
Sameer Sapkota <acidicsameer.namrmc008@gmail.com>
2025-11-24 12:57:08 +0545

    day 14 completed

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -1,31 +1,31 @@
-import React, { useState,useEffect } from "react";
+import React, { useEffect } from "react";
 
 const Hooks = () => {
-  const arr = [1,2,3,4,5,6]
- 
+//   const arr = [1,2,3,4,5,6]
+//   const [count,_setCount]=useState(9)
 
-  const [array,setArray] = useState([]);
-  //   function increase() {
-  //     setPrice(price + 1);
-  //   }
-  //   function decrease() {
-  //     setPrice(price - 1);
-  //   } 
+//   const [array,setArray] = useState([]);
+//   //   function increase() {
+//   //     setPrice(price + 1);
+//   //   }
+//   //   function decrease() {
+//   //     setPrice(price - 1);
+//   //   } 
  
-   function handlearr(){ 
-  setArray(arr)
+//    function handlearr(){ 
+//   setArray(arr)
      
-   } 
+//    } 
    useEffect(() => { 
-    console.log(array)
+    console.log( " rendered ")
     
-   }, [array]);
+   });
 
   return (
-    <div>
-      {/* <button onClick={increase}> Hooks </button>
-      <button onClick={decrease}> Hooks </button> */}
-      <button onClick={handlearr}> Hooks </button>
+    <div> 
+      <button > Hooks </button>
+      {/* <button onClick={decrease}> Hooks </button>
+      {/* <button onClick={handlearr}> Hooks </button> */} 
     </div>
   );
 };
